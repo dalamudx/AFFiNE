fb08f5ec150e02fbb402754e73e326b6d91e68fa
dalamudx <n386345222@hotmail.com>
2026-01-31 14:13:16 +0800

    feat: apply readonly mode

---
+++ packages/frontend/core/src/components/readonly-mode-toggle-button/index.tsx
@@ -0,0 +1,62 @@
+import { IconButton } from '@affine/component';
+import { DocService } from '@affine/core/modules/doc';
+import { EditorService } from '@affine/core/modules/editor';
+import { EditorSettingService } from '@affine/core/modules/editor-setting';
+import { useI18n } from '@affine/i18n';
+import { EditIcon, ViewIcon } from '@blocksuite/icons/rc';
+import { useLiveData, useService } from '@toeverything/infra';
+import { useCallback, useEffect, useState } from 'react';
+
+export const ReadonlyModeToggleButton = () => {
+  const t = useI18n();
+  const docService = useService(DocService);
+  const editorService = useService(EditorService);
+  const editorSettingService = useService(EditorSettingService);
+
+  const doc = docService.doc;
+  const editor = editorService.editor;
+  const mode = useLiveData(editor.mode$);
+  const settings = useLiveData(editorSettingService.editorSetting.settings$);
+
+  const [isReadonly, setIsReadonly] = useState(true);
+
+  useEffect(() => {
+    setIsReadonly(doc.blockSuiteDoc.readonly);
+  }, [doc.blockSuiteDoc.readonly]);
+
+  const handleToggle = useCallback(() => {
+    if (isReadonly) {
+      // Switch to edit mode
+      doc.blockSuiteDoc.readonly = false;
+      setIsReadonly(false);
+    } else {
+      // Switch to readonly mode (save)
+      doc.blockSuiteDoc.readonly = true;
+      setIsReadonly(true);
+    }
+  }, [isReadonly, doc.blockSuiteDoc]);
+
+  // Don't show button if:
+  // 1. Default readonly mode is not enabled
+  // 2. Current mode is edgeless
+  if (!settings.defaultReadonlyMode || mode === 'edgeless') {
+    return null;
+  }
+
+  const tooltipContent = isReadonly
+    ? t['com.affine.readonly-mode.edit.tooltip']?.() ||
+      'Click to switch to edit mode'
+    : t['com.affine.readonly-mode.readonly.tooltip']?.() ||
+      'Click to switch to readonly mode';
+
+  return (
+    <IconButton
+      size="20"
+      tooltip={tooltipContent}
+      data-testid="header-readonly-mode-toggle-button"
+      onClick={handleToggle}
+    >
+      {isReadonly ? <EditIcon /> : <ViewIcon />}
+    </IconButton>
+  );
+};
